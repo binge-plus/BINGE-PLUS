2a81e6ca1a185dd61ca6e452313f237dd1d37bfe
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-08-25 03:18:49 +0530

    fix background Running

--- start_streamlit.sh
+++ start_streamlit.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-streamlit run /home/binge+/streamlit/app.py --server.port 8501 --server.address 0.0.0.0
+
+# Activate the virtual environment
+source /home/binge+/streamlit/venv/bin/activate
+
+# Define the Streamlit application file (e.g., app.py)
+APP_FILE="app.py"
+
+# Start the Streamlit application in the background
+nohup streamlit run $APP_FILE --server.address 0.0.0.0 --server.port 8501 > streamlit.log 2>&1 &
