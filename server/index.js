4407deef1052447c90fc502537b6d6cd77d1683d
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-08-27 01:43:07 +0530

    port mongo

--- server/index.js
+++ server/index.js
@@ -1,44 +1,70 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import router from './routes/movieRouter.js';
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import router from './routes/movieRouter.js';
+// import cors from 'cors';
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // API route to fetch movies
+// app.use('/', router);
+
+// // Resolve __dirname in ES Modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Serve static files from the "bingeplus" directory
+// app.use(express.static(path.join(__dirname, '../')));
+
+// // Route for the root path
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../index.html'));
+// });
+
+// // MongoDB Connection String (Your MongoDB Atlas URI)
+// const mongoURI = 'mongodb+srv://bingemovies:CWuhFDboOssypOfD@binge.qvrdf.mongodb.net/BINGE';
+
+// // Connect to MongoDB Atlas
+// mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch((err) => console.error('Failed to connect to MongoDB', err));
+
+// // Start the server
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
 import cors from 'cors';
+import express from 'express';
+import router from './routes/movieRouter.js';
+
 
 const app = express();
+const PORT = 8080;
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+
 // API route to fetch movies
+
+
 app.use('/', router);
 
-// Resolve __dirname in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files from the "bingeplus" directory
-app.use(express.static(path.join(__dirname, '../')));
-
-// Route for the root path
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
-
-// MongoDB Connection String (Your MongoDB Atlas URI)
-const mongoURI = 'mongodb+srv://bingemovies:CWuhFDboOssypOfD@binge.qvrdf.mongodb.net/BINGE';
-
-// Connect to MongoDB Atlas
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Failed to connect to MongoDB', err));
-
-// Start the server
-const PORT = process.env.PORT || 8080;
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
