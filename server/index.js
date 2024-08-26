0beced22662c2792e86ddd9bd1c8612995234404
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-08-27 00:41:36 +0530

    fix js

--- server/index.js
+++ server/index.js
@@ -1,27 +1,3 @@
-import cors from 'cors';
-import express from 'express';
-import router from './routes/movieRouter.js';
-
-
-// const app = express();
-// const PORT = 8080;
-
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-
-// // API route to fetch movies
-
-
-// app.use('/', router);
-
-// // Start server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
 const express = require('express');
 const path = require('path');
 const app = express();
