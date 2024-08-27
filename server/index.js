d774497d8bb66ee721b8f55eb46e857382b44e0d
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-08-27 10:54:46 +0530

    Update a Minor Fix

--- server/index.js
+++ server/index.js
@@ -35,5 +35,5 @@ const mongoURI = 'mongodb+srv://bingemovies:CWuhFDboOssypOfD@binge.qvrdf.mongodb
 // Start the server
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://34.45.6.128:5555:${PORT}`);
 });
