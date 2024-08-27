3c077ef020e4a795340de4f5dc53303280c8c199
Ankush Kumar Jha <jhaankushcpp@gmail.com>
2024-08-27 10:00:47 +0530

    Added URL

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ import cors from 'cors';
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 // API route to fetch movies
