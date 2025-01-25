eea23b4a5a3eada278a06b80b53f5715b4014464
ankushjha-aj <jhaankushcpp@gmail.com>
2025-01-25 23:39:00 +0530

    Adding Artifacts Repo for storing Application Docker Images

--- modules/artifacts-registry/versions.tf
+++ modules/artifacts-registry/versions.tf
@@ -6,9 +6,5 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 4.80.0"
     }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.80.0"
-    }
   }
 }
